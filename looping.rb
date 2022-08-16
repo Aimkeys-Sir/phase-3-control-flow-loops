e41fa7f0ef4afe41dfb7ef569ba304fcaa46b1c6
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-08-16 13:39:11 +0300

    commit tree e71adcb9560e34530560d66c9031d8f6ba834142

--- looping.rb
+++ looping.rb
@@ -1,7 +1,13 @@
 def happy_new_year
   # your code here
+  i=10
+  until i==0
+    puts i
+    i-=1
+  end
+  puts "Happy New Year!"
 end
-
+happy_new_year
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
@@ -17,8 +23,18 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |n|
+    puts fizzbuzz(n)
+  end
 end
 
 def reverse_string(str)
   # your code here
+  rev=[]
+  str.chars.each do |char|
+    rev.unshift(char)
+  end
+  rev.join
 end
+puts reverse_string("Oh my")
+fizzbuzz_printer
